a69b19b9d2a58eaee2bcd86358bfb31f989e2240
Ryan <ryanjamesyang@gmail.com>
2024-05-15 19:27:19 +0800

    reorganize file structure

--- src/components/pages/UserPages/RecordsPage.vue
+++ src/components/pages/UserPages/RecordsPage.vue
@@ -1,3 +1,3 @@
 <template>
-<h1>Recordspage</h1>
+<h1>Recordspage 買賣紀錄</h1>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,29 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// General Pages
 import HomePage from "./components/pages/HomePage.vue";
 import NotFound from "./components/pages/NotFound.vue";
 import AboutUs from "./components/pages/AboutUs.vue";
-import ProductsPage from "./components/pages/productsPage.vue";
-import ProfilePage from "./components/pages/UserPages/ProfilePage.vue";
+
+// User Pages
 import UserCart from "./components/pages/UserPages/UserCart.vue";
+import ProfilePage from "./components/pages/UserPages/ProfilePage.vue";
 import RecordsPage from "./components/pages/UserPages/RecordsPage.vue";
-import MarketPlace from "./components/pages/UserPages/MarketPlace.vue";
-import newProduct from "./components/products/newProduct.vue";
+import IndivMarket from "./components/pages/UserPages/IndivMarket.vue";
+
+// Product Pages
+import ProductsPage from "./components/pages/ProductPages/productsPage.vue";
+import newProduct from "./components/pages/ProductPages/newProduct.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", redirect: "/Home" },
+    // General Pages
+    { path: "/", redirect: "/home" },
     { path: "/home", component: HomePage },
-    { path: "/about", component: AboutUs },
-    { path: "/products", component: ProductsPage },
-    { path: "/profile", component: ProfilePage },
-    { path: "/cart", component: UserCart },
     { path: "/:notFound(.*)", component: NotFound },
+    { path: "/about", component: AboutUs },
+
+    // User Pages
+    { path: "/cart", component: UserCart },
+    { path: "/IndivMarket", component: IndivMarket },
     { path: "/records", component: RecordsPage },
-    { path: "/marketplace", component: MarketPlace },
-    { path: "/newproduct", component: newProduct},
-    
+    { path: "/profile", component: ProfilePage },
+
+    // Product Pages
+    { path: "/products", component: ProductsPage },
+    { path: "/newproduct", component: newProduct },
   ],
 });
 
