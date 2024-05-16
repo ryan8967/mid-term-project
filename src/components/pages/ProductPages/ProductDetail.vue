a704d15ff14e0ff55814f7e4ef4d3a42e1db7704
its6000dollars <chian.666w@gmail.com>
2024-05-17 04:31:16 +0800

    Update ProductDetail.vue

--- src/components/pages/ProductPages/ProductDetail.vue
+++ src/components/pages/ProductPages/ProductDetail.vue
@@ -1,8 +1,8 @@
-<!--還剩一點，待更新，有問題或要改拜託一定要告訴我-->
+<!--有問題或要改告訴我-->
 <template>
   <div class="product-card">
     <img
-      :src="product.image"
+      src="@/assets/images/guitar.png"
       alt="Product Image"
       class="product-image"
       style="
@@ -18,7 +18,7 @@
     <div class="rectangle" style="left: 759px">
       <span class="text">其他</span>
     </div>
-    <div class="rectangle" style="left: calc(759px + 102px + 45px)">
+    <div class="rectangle" style="left: calc(759px + 102px + 13px)">
       <span class="text">吉他</span>
     </div>
     <div class="line"></div>
@@ -34,7 +34,7 @@
         <span v-for="n in product.rating" :key="n" class="star">&#9733;</span>
       </div>
         <div class="add-to-cart-btn">
-          <button @click="addToCart" style="position: absolute; bottom: 20px; right: 20px; width: 200px; height: 50px; padding: 10px 20px; font-size: 20px; background-color: #ffffff; color: black; border: 3px solid #d5ba9c; border-radius: 30px; cursor: pointer; box-shadow: 2px 2px 3px #888888;">加入購物車</button>
+          <button @click="addToCart" style="position: absolute; bottom: 40px; right: -780px; width: 200px; height: 50px; padding: 10px 20px; font-size: 20px; background-color: #ffffff; color: black; border: 3px solid #d5ba9c; border-radius: 30px; cursor: pointer; box-shadow: 2px 2px 3px #888888;">加入購物車</button>
         </div>
     </div>
     <div class="item-details" style="width: 610px; margin-top: 30px">
@@ -45,6 +45,23 @@
         <p class="description">{{ product.description }}</p>
       </ul>
     </div>
+    <!-- New colored block at the bottom left corner -->
+    <div class="staffInformation">
+      <p class="ratinggg">{{ product.rating.toFixed(1) }}</p>
+      <div class="sub-block left">
+        COOL STUFF
+      </div>
+      <div class="sub-block right">
+        私訊
+      </div>
+    </div>
+    <div>
+      <p style="position: absolute; bottom: 110px; right: -740px; font-size: 20px; color:rgb(186, 116, 131);">我有興趣!(N)</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Heart_corazón.svg" alt="heart Icon" style="position: absolute; bottom: 106px; right: -620px; width: 30px; height: 30px;">
+    </div>
+<div class="add-to-cart-btn">
+  <button @click="addToCart" style="position: absolute; bottom: 40px; right: -780px; width: 200px; height: 50px; padding: 10px 20px; font-size: 20px; background-color: #ffffff; color: black; border: 3px solid #d5ba9c; border-radius: 30px; cursor: pointer; box-shadow: 2px 2px 3px #888888;">加入購物車</button>
+</div>
   </div>
 </template>
 
@@ -68,16 +85,20 @@ export default {
         price: "¥87,800",
         description:
           "Alder body with gloss finish, Three Player Series single-coil Stratocaster pickups, 'Modern C'-shaped neck profile",
-        //image: "path_to_image.jpg",
         rating: 5,
       },
     };
   },
+  
   methods: {
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    },
     addToCart() {
       // Add to cart logic
       alert("Added to cart!");
     },
+
   },
 };
 </script>
@@ -111,13 +132,19 @@ export default {
 }
 
 .star {
-  color: gold;
+  position: relative;
+  bottom: -120px; 
+  right: -350px; 
+  font-size: 35px;
+  color: #FFF384;
+  -webkit-text-stroke: 2.5px #d1b596;
+  z-index: 1; 
 }
 
 .rectangle {
-  width: 102px;
-  height: 32px;
-  background-color: #d5ba9c;
+  width: 85px;
+  height: 29px;
+  background-color: #E2DBC9;
   border-radius: 30px;
   position: absolute;
   top: 135px;
@@ -145,11 +172,54 @@ export default {
   left: 730px;
   background-color: #c69f76;
 }
-
 .item-details {
   position: absolute;
   top: 200px;
   left: 730px;
   font-size: 18px;
+}
+.staffInformation {
+  position: absolute;
+  bottom: 40px;
+  left: 100px;
+  width: 720px;
+  height: 60px;
+  background-color: #E2DBC9;
+  border-radius: 25px;
+  font-size: 23px;
+  line-height: 42px;
+  text-align: center;
+  box-shadow: 2px 2px 3px #888888;
+}
+.sub-block {
+  position: absolute;
+  bottom: 10px; 
+  height: 40px;
+  border-radius: 25px;
+  justify-content: center;
+  font-size: 23px;
+  text-align: center;
+  line-height: 42px;
+}
+.left {
+  width: 240px;
+  left: 10px; 
+  background-color: #ffffff; 
+}
+.right {
+  width: 180px;
+  right: 10px;
+  background-color: #ffffff;
+}
+.ratinggg {
+  position: absolute;
+  bottom: 10px;
+  left: 465px; 
+  font-size: 27px;
+  line-height: 42px;
+  text-align: center;
+  z-index: 1; 
+  color: #C69F76;
+  /*-webkit-text-stroke: 3px #C69F76;*/
 }
 </style>
