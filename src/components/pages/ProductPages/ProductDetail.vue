304e4de3ceebabe9d2107d15f0b418af34f32780
Ryan <ryanjamesyang@gmail.com>
2024-05-16 21:59:14 +0800

    add prop to product detail page

--- src/components/pages/ProductPages/ProductDetail.vue
+++ src/components/pages/ProductPages/ProductDetail.vue
@@ -1,17 +1,30 @@
 <!--還沒寫完-->
 <template>
   <div class="product-card">
-    <img :src="product.image" alt="Product Image" class="product-image" style="width: 600px; height: 600px; border-radius: 15px; position: relative;"/>
-      <h1 class="product-name" style="top: 70px;">{{ product.name }}</h1>
-    <div class="rectangle" style="left: 759px;">
+    <img
+      :src="product.image"
+      alt="Product Image"
+      class="product-image"
+      style="
+        width: 600px;
+        height: 600px;
+        border-radius: 15px;
+        position: relative;
+      "
+    />
+    <h1 class="product-name" style="top: 70px">{{ product.name }}</h1>
+    <div class="rectangle" style="left: 759px">
       <span class="text">其他</span>
     </div>
-    <div class="rectangle" style="left: calc(759px + 102px + 45px);">
+    <div class="rectangle" style="left: calc(759px + 102px + 45px)">
       <span class="text">吉他</span>
     </div>
     <div class="line"></div>
     <div class="product-details">
-      <div class="price-container" style="position: absolute; left: calc(759px + 450px);">
+      <div
+        class="price-container"
+        style="position: absolute; left: calc(759px + 450px)"
+      >
         <p class="price">{{ product.price }}</p>
       </div>
       <div class="ratings">
@@ -20,13 +33,12 @@
       </div>
       <button @click="addToCart">加入購物車</button>
     </div>
-    <div class="item-details" style="width: 610px; margin-top: 30px;">
-      <ul style="font-size: 25px; line-height:2.25;">
+    <div class="item-details" style="width: 610px; margin-top: 30px">
+      <ul style="font-size: 25px; line-height: 2.25">
         <li>商品狀況 : 九成新</li>
         <li>數量: 1</li>
-        <li>商品詳情描述 : </li>
-        <p class="description" >{{ product.description }}</p>
-        
+        <li>商品詳情描述 :</li>
+        <p class="description">{{ product.description }}</p>
       </ul>
     </div>
   </div>
@@ -34,25 +46,36 @@
 
 <script>
 export default {
-  name: 'ProductCard',
+  props: [
+    "id",
+    "image",
+    "title",
+    "tag",
+    "condition",
+    "price",
+    "quantity",
+    "description",
+  ],
+  name: "ProductCard",
   data() {
     return {
       product: {
         name: "Fender Stratocaster",
         price: "¥87,800",
-        description: "Alder body with gloss finish, Three Player Series single-coil Stratocaster pickups, 'Modern C'-shaped neck profile",
+        description:
+          "Alder body with gloss finish, Three Player Series single-coil Stratocaster pickups, 'Modern C'-shaped neck profile",
         //image: "path_to_image.jpg",
-        rating: 5
-      }
-    }
+        rating: 5,
+      },
+    };
   },
   methods: {
     addToCart() {
       // Add to cart logic
-      alert('Added to cart!');
-    }
-  }
-}
+      alert("Added to cart!");
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -104,8 +127,7 @@ export default {
 
 .price-container {
   position: absolute;
-  top: 113px; 
-  
+  top: 113px;
 }
 
 .price {
@@ -113,11 +135,11 @@ export default {
 }
 .line {
   position: absolute;
-  top: 180px; 
+  top: 180px;
   width: 633px;
   height: 2px;
   left: 730px;
-  background-color: #C69F76;
+  background-color: #c69f76;
 }
 
 .item-details {
