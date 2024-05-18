131877d3378bb5ed561a8918dd61cb6e1427aa79
Steinbrech <alex21083742@gmail.com>
2024-05-18 11:59:33 +0800

    addeventlistener in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import { createApp } from 'vue'
-import router from './router.js';
-import App from './App.vue'
+import { createApp } from 'vue';
+import router from './router';
+import App from './App.vue';
+import axios from 'axios';
 
-const app = createApp(App)
+document.addEventListener('DOMContentLoaded', function () {
+    const token = document.querySelector('meta[name="csrf-token"]');
+    if (token) {
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = token.getAttribute('content');
+    } else {
+        console.error('CSRF token not found: Check that your meta tag is correct');
+    }
+});
 
+const app = createApp(App);
 app.use(router);
-
 app.mount('#app');
