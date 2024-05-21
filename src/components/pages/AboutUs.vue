4a518688730cbc3e988b157f38d6387a8c343a5d
its6000dollars <chian.666w@gmail.com>
2024-05-21 16:00:49 +0800

    Update AboutUs.vue

--- src/components/pages/AboutUs.vue
+++ src/components/pages/AboutUs.vue
@@ -1,9 +1,10 @@
-<!--還沒寫完，美編還在思考中，下面也還會再加點東西-->
+<!--我覺得有點小空，有什麼想到的美編拜託告訴我，有要改的也可以跟我說-->
 <template>
   <div>
     <h1>About us</h1>
     <div class="container">
       <div class="content-section">
+
         <div class="text">
           <h2>中大師生專屬</h2>
           <p>提供更實惠的購買選擇，同時推動舊物的循環利用，減少浪費</p>
@@ -13,7 +14,7 @@
         </div>
       </div>
 
-      <div class="content-section">
+      <div class="content-section-right">
         <div class="image">
           <img src="@/assets/images/aboutus-2.jpg" alt="Image for 保障交易安全">
         </div>
@@ -33,7 +34,7 @@
         </div>
       </div>
 
-      <div class="content-section">
+      <div class="content-section-right">
         <div class="image">
           <img src="@/assets/images/aboutus-4.jpg" alt="Image for 學生團隊打造">
         </div>
@@ -42,6 +43,30 @@
           <p>理解並關心用戶需求，致力打造更便利的交易平台</p>
         </div>
       </div>
+    </div>
+    <h1><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Our team</a></h1>
+    <div class="team-images">
+      <div class="team-image">
+        <img src="@/assets/images/team-1.jpg" alt="Team member 1">
+        <div class="image-description">
+          <p>"中央大學學生團隊"</p>
+          <p>兄弟 轉身</p>
+        </div>
+      </div>
+      <div class="team-image">
+        <img src="@/assets/images/team-2.jpg" alt="Team member 2">
+        <div class="image-description">
+          <p>"中央大學學生團隊"</p>
+          <p>兄弟 你好香</p>
+        </div>
+      </div>
+      <div class="team-image">
+        <img src="@/assets/images/team-3.jpg" alt="Team member 3">
+        <div class="image-description">
+          <p>"中央大學學生團隊"</p>
+          <p>兄弟 我沒了</p>
+        </div>
+       </div>
     </div>
   </div>
 </template>
@@ -59,19 +84,24 @@ export default {
 
 img {
   width: 400px;
-  height: 400px;
+  height: 300px;
+  aspect-ratio: 4 / 3;
+  height: auto;
   display: block;
   max-width: 100%;
-  height: auto;
   margin: 0 auto;
   padding: 10px;
 }
 
 h1 {
-  text-align: center;
-  font-size: 40px;
-  margin:5%;
-  /*margin-bottom: 10%;*/
+  display: flex;
+  align-items: center; /* 垂直置中 */
+  justify-content: center; /* 水平置中 */
+  font-size: 50px;
+  margin: 5%;
+  /* margin-bottom: 10%; */
+  border-bottom: 4px solid #7b6d64;
+  width:auto;
 }
 
 .container {
@@ -90,7 +120,22 @@ h1 {
   max-width: 1200px;
   border-radius: 5px;
   padding: 20px;
-  background-color: #f4f2ec;
+  background-color: #f7eac369;
+  /*border: 2.5px solid #7b6d644b;*/
+  animation: slideFromLeft 2s ease forwards;
+}
+.content-section-right {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1200px;
+  border-radius: 5px;
+  padding: 20px;
+  background-color: #b1927243;
+  /*border: 2.5px solid #7b6d644b;*/
+  animation: slideFromRight 2s ease forwards;
 }
 
 .text {
@@ -111,13 +156,52 @@ h1 {
   align-items: center;
 }
 
+.team-images {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 3% 20% 5% 20%;
+  max-width: 100%;
+  height: auto;
+}
+/*我知道視窗縮小的時候圖片會壓縮，等我有精力會回來調*/
+.team-image img {
+  flex: 1;
+  width: 250px;
+  height: 300px;
+  margin: 3%;
+}
+.image-description {
+  text-align: center;
+}
+
 h2 {
-  font-size: 28px;
+  font-size: 30px;
   margin-bottom: 10px;
 }
 
 p {
-  font-size: 20px;
+  font-size: 22px;
   line-height: 1.5;
+}
+@keyframes slideFromLeft {
+  from {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@keyframes slideFromRight {
+  from {
+    opacity: 0;
+    transform: translateX(200%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 }
 </style>
