593a76af6bfddd0d28f1c75856eba7091153a5b9
Ryan <ryanjamesyang@gmail.com>
2024-05-22 13:45:26 +0800

    fix dropdown content

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -186,13 +186,14 @@ a {
 .profile-dropdown {
   position: relative;
   display: inline-block;
+  padding-right: 10px;
 }
 
 .dropdown-content {
   display: block;
   position: absolute;
   background-color: #f9f9f9;
-  min-width: 160px;
+  min-width: 100px;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
 }
