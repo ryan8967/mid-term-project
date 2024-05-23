de0acd92e2d7430526158832f054b22c0612546c
joe <joelin9201@gmail.com>
2024-05-23 11:41:14 +0800

    update 3CPage's images

--- src/components/pages/ProductPages/Category/3C/3CPage.vue
+++ src/components/pages/ProductPages/Category/3C/3CPage.vue
@@ -2,6 +2,10 @@
   <div class="container">
     <h1>3C 用品都在這</h1>
   </div>
+  <img
+    src="@/assets\images\3c product.jpg"
+    alt="Image for 3c"
+  />
   <div class="products">
     <ProductCard
       v-for="prod in formattedProducts"
